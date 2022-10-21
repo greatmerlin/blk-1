5ba5171399cc79750a56b552b03b9fb1b496f23b
greatmerlin <gmbaxevanos@gmx.ch>
2022-10-21 21:32:28 +0200

    added wallet config in truffle config

--- lottery/truffle-config.js
+++ lottery/truffle-config.js
@@ -64,7 +64,7 @@ module.exports = {
     // },
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
-    rinkeby: {
+    /* rinkeby: {
       provider: () => new HDWalletProvider({
         privateKeys: private_keys,
         providerOrUrl: `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`,
@@ -72,6 +72,19 @@ module.exports = {
       }),
       network_id: 4,       // Rinkeby's id
       gas: 5500000,        // Rinkeby has a lower block limit than mainnet
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    }, */
+    goerli: {
+      provider: () => new HDWalletProvider({
+      privateKeys: private_keys,
+      providerOrUrl: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      numberOfAddresses: 1
+    }),
+
+      networkId: 5,     // Goerli's id
+      gas: 7500000,
       confirmations: 2,    // # of confs to wait between deployments. (default: 0)
       timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
       skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
